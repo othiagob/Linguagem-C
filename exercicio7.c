f0bf42d911fd8f8a930fd7e87a23ee053d558265
Thiago S. Borghardt <thiago.backend@gmail.com>
2025-05-20 21:29:09 -0400

    exercicio7

--- exercicio7.c
+++ exercicio7.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    char caractere;
-    
-    printf("Digite um caractere: ");
-    scanf("%c", &caractere);
-    
-    printf("O valor inteiro do caractere '%c' é: %d\n", caractere, caractere);
-    
-    return 0;
+  char caractere;
+
+  printf("Digite um caractere: ");
+  scanf("%c", &caractere);
+
+  printf("O valor inteiro do caractere '%c' é: %d\n", caractere, caractere);
+
+  return 0;
 }
